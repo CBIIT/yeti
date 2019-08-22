2373b08785c719621eb279890f765cb5ca3e5a9a
Mark A. Jensen <maj-git@fortinbras.us>
2019-08-21 23:01:18 -0400

    feat(delete): WIP
    control changes from add to remove with meta key (when hovering
    over control)
    on delete, need to replace deleted thing with appropriate object select

--- yedit/public/javascripts/actions.js
+++ yedit/public/javascripts/actions.js
@@ -7,18 +7,9 @@ $(function () {
   $(".yaml-obj-key")
     .dblclick( hider )
   $(".yaml-obj-ent-control")
-    .text("+")
-    .click( (e) => {
-      e.stopPropagation
-      insert_obj_ent(e.target.closest(".yaml-obj-ent"))
-    })
+    .each( edit_control_setup )
   $(".yaml-arr-elt-control")
-    .text("+")
-    .click( (e) => {
-      e.stopPropagation
-      insert_arr_elt(e.target)
-    })
-  console.log(parse_dom())
+    .each( edit_control_setup )
 })
 
 function insert_obj_ent (tgt) {
@@ -51,26 +42,18 @@ function create_obj_ent (indent) {
   create_type_select(indent).appendTo(elt.find('.yaml-obj-val'))
   $(elt).find(".yaml-obj-key")
     .dblclick( hider )
-  $(elt).find(".yaml-obj-ent-control")
-    .click( (e) => {
-      e.stopPropagation
-      insert_obj_ent(e.target.closest(".yaml-obj-ent"))
-    })
+  $(elt).find(".yaml-obj-ent-control").each( edit_control_setup )
   return elt
 }
 
 function create_arr_elt (indent) {
   let elt = $('<div class="yaml-arr-elt">' +
 	      '<span class="yaml-arr-elt-mrk">-</span>'+
-	      '<span class="yaml-arr-elt-control"> +</span>' +
+	      '<span class="yaml-arr-elt-control"></span>' +
 	      '<span class="yaml-status"></span>'+
 	      '</div>');
   create_type_select(indent).insertAfter(elt.find('.yaml-arr-elt-mrk'))
-  $(elt).find(".yaml-arr-elt-control")
-    .click( (e) => {
-      e.stopPropagation
-      insert_arr_elt(e.target)
-    })
+  $(elt).find(".yaml-arr-elt-control").each( edit_control_setup )
   return elt
 }
 function create_obj (indent) {
@@ -126,6 +109,48 @@ function hider (e) {
       else {
 	$(stat).text("")
       }
+    })
+}
+
+function edit_control_setup () {
+  let cls;
+  if ($(this).hasClass('yaml-obj-ent-control')) cls='yaml-obj-ent'
+  if ($(this).hasClass('yaml-arr-elt-control')) cls='yaml-arr-elt'
+  $(this)
+    .text("•")
+    .hover(
+      function (e) {
+	$(this).text("⊕")
+	let $elt = $(this)
+	$(document).on(
+	  "keydown.yaml",
+	  function (f) {
+	    if (f.key == 'Meta') $elt.attr('style','color:red').text("⊗")
+	  }).on(
+	    "keyup.yaml",
+	    function (f) {
+	      if (f.key == 'Meta') $elt.removeAttr('style').text("⊕")
+	    })
+      },
+      function (e) {
+	$(this).removeAttr('style').text("•")
+	$(document).off("keydown.yaml").off("keyup.yaml")
+      } )
+    .click( (e) => {
+      e.stopPropagation
+      if ($(e.target).text() == "⊕")
+	switch (cls) {
+	case 'yaml-obj-ent':
+	  insert_obj_ent(e.target.closest('.'+cls))
+	  break
+	case 'yaml-arr-elt':
+	  insert_arr_elt(e.target.closest('.'+cls))
+	  break
+	default:
+	  break
+	}
+      else if ($(e.target).text() == "⊗")
+	$(e.target).closest('.'+cls).remove()
     })
 }
 
