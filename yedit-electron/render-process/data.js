c1f3aa70770f66278c57d7404d72efa693db8ac7
Mark A. Jensen <mark.jensen@nih.gov>
2019-09-22 22:43:04 -0400

    feat(yaml by id) : unit tests WIP

--- yedit-electron/render-process/data.js
+++ yedit-electron/render-process/data.js
@@ -3,6 +3,7 @@ const yaml = require('yaml')
 const ytypes = require('yaml/types')
 const _ = require('lodash')
 
+
 // ydoc isa yaml.Document
 // return d3.hierarchy
 function render_data(ydoc) {
@@ -122,6 +123,18 @@ function children (n) {
   }
 }
 
+// yaml Document mod by id API
+// create_node(obj|arr|scal) - create a new node with an id
+// create_pair_node(key, obj|arr|scal) - create a new pair (key, value) for inserting into a MAP
+// get_node_by_id(id)
+// get_parent_by_id(id) - get parent of node 'id'
+// remove_node_by_id(id) - remove node 'id' from document
+// append_to_id(id, node, [prepend]) - append/prepend a node to a MAP or SEQ of 'id'
+// insert_at_id(id, node, [before])  - insert after/before node 'id' as a sibling in a MAP or SEQ
+//
+// __walk(pnode, function, first) - apply function to each node, starting at and including pnode
+//                                - (call as ydoc.__walk(pnode, function, true))
+
 function instrument_ydoc(ydoc) {
   function* idgen() {
     i = 0;
@@ -149,16 +162,22 @@ function instrument_ydoc(ydoc) {
     }
   }
 
-  ydoc.__walk = function (n,fn) {
-    fn(n)
+  // breadth-first walk 
+  ydoc.__walk = function (n,fn,first) {
+    if (first) {
+      fn(n)
+    }
     switch (n.type) {
     case 'PAIR':
+      fn(n.value)
       this.__walk(n.value,fn)
       break
     case 'MAP':
+      n.items.forEach( fn )
       n.items.forEach( (d) => { this.__walk(d, fn) })
       break
     case 'SEQ':
+      n.items.forEach( fn )
       n.items.forEach( (d) => { this.__walk(d, fn) })
       break
     default:
@@ -166,14 +185,19 @@ function instrument_ydoc(ydoc) {
     }
   }
   ydoc._add_ynode_ids = function (node) {
-    this.__walk(node, (n) => {n.id = this._idgen.next().value})
+    // node.id = this._idgen.next().value
+    this.__walk(node, (n) => {n.id = this._idgen.next().value}, true)
     return true
   }
   ydoc._set_parent_ids = function (node) {
-    this.__walk(node, (n) => { let c = this.__children(n) ; if (c) c.forEach( (d) => {  if (d) d.parent_id = n.id } ) } )
+    this.__walk(node, (n) => { let c = this.__children(n) ; if (c) c.forEach( (d) => {  if (d) d.parent_id = n.id } ) },true )
   }
-  ydoc._add_ynode_ids = function (node) {  ydoc._type_ynode = function (node) {
-    this.__walk(node, (n) => {
+  ydoc._type_ynode = function (node) {
+    add_type(node)
+    this.__walk(node, add_type )
+    function add_type (n) {
+      if (n.type)
+        return
       if (n instanceof ytypes.YAMLMap)
         n.type='MAP'
       else if (n instanceof ytypes.YAMLSeq)
@@ -184,34 +208,62 @@ function instrument_ydoc(ydoc) {
         n.type='PAIR'
       else
         console.log('bork')
-    })
-  } }  
+    }
+  }
   ydoc._index_ynode_ids = function (node) {
     if (!this.index)
       this.index = {}
-    this.__walk(node, (n) => {this.index[n.id] = n})
+    // this.index[node.id] = node
+    this.__walk(node, (n) => {this.index[n.id] = n}, true)
     return true
   }
   ydoc._add_ynode_ids(ydoc.contents)
+  ydoc._type_ynode(ydoc.contents)
+  ydoc._set_parent_ids(ydoc.contents)
   ydoc._index_ynode_ids(ydoc.contents)
-  ydoc.get_node_by_id = function (id) { return this.index[id] }
+  ydoc.get_node_by_id = function (id) {
+    if (!this.index[id]) {
+      console.error(`No node with id ${id}`)
+    }
+    return this.index[id] || false
+  }
   ydoc.get_parent_by_id = function (id) {
-    let p_id = this.get_node_by_id(id).parent_id
+    let n = this.get_node_by_id(id)
+    if (!n) {
+      console.error(`No node with id ${id}`)
+      return false
+    }
+    let p_id = n.parent_id
     if (p_id)
       return this.get_node_by_id(p_id)
     return
   }
-  ydoc.create_node = function () {
-    let n = yaml.createNode(...arguments)
+  ydoc.create_node = function (...args) {
+    let n = yaml.createNode(...args,true)
     this._type_ynode(n)
     this._add_ynode_ids(n)
     this._set_parent_ids(n)
+    this._index_ynode_ids(n)
     return n
   }
+  
+  ydoc.create_pair_node = function (key,...args) {
+    let n = this.create_node(...args)
+    let k = new ytypes.Scalar()
+    k.type = 'PLAIN'
+    k.value = key
+    let pr = new ytypes.Pair(k,n)
+    this._add_ynode_ids(pr)
+    this._index_ynode_ids(pr)
+    return pr
+  }
+    
   ydoc.index_node = function (node) { this._index_ynode_ids(node) }
   ydoc.remove_node_by_id = function (id) {
     let n=this.get_node_by_id(id) ;
     if (!n) return ;
+    let child_ids=[]
+    this.__walk(n, (nn) => { let c = this.__children(nn) ; if (c) c.forEach( (d) => { child_ids.push(d.id) } ) },true )
     if (!n.parent_id) {
       console.error("ydoc: can't remove root node")
       return
@@ -224,13 +276,22 @@ function instrument_ydoc(ydoc) {
     else if (pn.type == 'SEQ') {
       let i = pn.items.indexOf(n);
       n.parent_id=null
-      pn.value.delete(i)
+      pn.delete(i)
     }
+    delete this.index[id]
+    child_ids.forEach( (cid) => { delete this.index[cid] } )
+    return true
   }
   ydoc.insert_at_id = function(id,node,before) {
     let n=this.get_node_by_id(id)
-    if (!n || !node)
+    if (!n) {
+      console.error(`No node with id ${id}`)
       return false
+    }
+    if (!node) {
+      console.error("Node to insert undefined")
+      return false
+    }
     if (!node.id) {
       console.error('Node to insert does not have an id')
       return false
@@ -244,17 +305,22 @@ function instrument_ydoc(ydoc) {
       console.error(`Can't insert: node ${id} (type ${n.type}) can't have siblings`)
       return false
     }
+    if (pn.type=='MAP' && !(node.type=='PAIR')) {
+      console.error(`New node must be type 'PAIR' to insert into 'MAP'`)
+      return false
+    }
     let i = pn.items.indexOf(n)
     if (i<0) {
       console.error(`Indexing error: node ${n.id} should be a child of node ${pn.id}, but isn't`)
       return false
     }
+    node.parent_id=pn.id
     if (before) {
       pn.items.splice(i,0,node)
     }
     else {
-      if (i == a.length-1) {
-        pn.items.splice(a.length,0,node)
+      if (i == pn.items.length-1) {
+        pn.items.splice(pn.items.length,0,node)
       }
       else {
         pn.items.splice(i+1,0,node)
@@ -264,8 +330,10 @@ function instrument_ydoc(ydoc) {
   }
   ydoc.append_to_id = function(id,node,prepend) {
     let n = this.get_node_by_id(id)
-    if (!n || !node)
+    if (!n || !node) {
+      console.error(`No node with id ${id}`)
       return false
+    }
     if (!node.id) {
       console.error('Node to insert does not have an id')
       return false
@@ -278,17 +346,18 @@ function instrument_ydoc(ydoc) {
       console.error('can only append nodes of type PAIR to a MAP')
       return false
     }
+
+
     if (prepend) {
       n.items.splice(0,0,node)
     }
     else {
       n.items.splice(n.items.length,0,node)
     }
+    node.parent_id = n.id
     return true
   }
 }
-
-
 
 
 exports.render_data=render_data
---
+++ yedit-electron/render-process/data.test.js
@@ -0,0 +1,192 @@
+const yaml = require('yaml')
+const ytypes = require('yaml/types')
+const fs = require('fs')
+const ydata = require('./data.js')
+
+yf = fs.readFileSync('test.yaml','utf-8')
+yd = yaml.parseDocument(yf)
+ydata.instrument_ydoc(yd)
+
+// get_node_by_id
+// create_node
+// index_node
+// remove_node_by_id
+// insert_at_id
+// append_to_id
+
+
+// yd.__walk( yd.contents, (d) => { console.log(d.type, d.id, d.parent_id,
+//                                             d.key ? d.key.value : (d.value ? d.value : "")) })
+// yields following test structure:
+// PAIR n1 n0 red
+// PAIR n2 n0 blue
+// PAIR n3 n0 yellow
+// MAP n4 n1 
+// PAIR n5 n4 rojo
+// PAIR n6 n4 azul
+// PAIR n7 n4 amarillo
+// PLAIN n8 n5 1
+// SEQ n9 n6 
+// PLAIN n10 n9 a
+// PLAIN n11 n9 b
+// PLAIN n12 n9 c
+// PLAIN n13 n7 
+// MAP n14 n2 
+// PAIR n15 n14 blau
+// PAIR n16 n14 rot
+// PAIR n17 n14 gelb
+// PLAIN n18 n15 5
+// PLAIN n19 n16 6
+// SEQ n20 n17 
+// MAP n21 n20 
+// MAP n22 n20 
+// PAIR n23 n21 gruen
+// PAIR n24 n21 braun
+// PLAIN n25 n23 7
+// PLAIN n26 n24 8
+// PAIR n27 n22 schwarz
+// PLAIN n28 n27 kopf
+// MAP n29 n3 
+// PAIR n30 n29 bleu
+// SEQ n31 n30 
+// PLAIN n32 n31 rose
+// PLAIN n33 n31 violette
+// PLAIN n34 n31 brun
+
+test('yd is yaml Doc', () =>{
+  expect(yd).toBeInstanceOf(yaml.Document)
+})
+test('ydoc has red toplevel', () => {
+  expect(yd.contents.has('red')).toBeTruthy()
+})
+test('all elts have an id', () => {
+  expect( () => {
+    yd.__walk( yd.contents, (d) => { if (!d.id) { throw new Error('no id found on elt') } }, true)
+  } ).not.toThrow()
+})
+test('all non-root elts have a parent_id', () => {
+  expect( () => {
+    yd.__walk( yd.contents, (d) => { if (!d.parent_id) { throw new Error(`no parent_id found on elt ${d.id}`) } })
+  } ).not.toThrow()
+})
+test('n0 is MAP', () => {
+  expect( yd.get_node_by_id('n0').type ).toBe('MAP')
+})
+test('n1-n3 red, blue, yellow', () => {
+  expect( ['n1','n2','n3'].map( n => yd.get_node_by_id(n).key.value) ).toStrictEqual(['red','blue','yellow'])
+})
+test('n23: gruen', () => {
+  expect( yd.get_node_by_id('n23').key.value ).toBe('gruen')
+})
+test('gruen grandparent is SEQ', () => {
+  expect( yd.get_parent_by_id(yd.get_parent_by_id('n23').id).type ).toBe('SEQ')
+})
+test('remove violette node (mbr of SEQ)', () => {
+  let pn = yd.get_parent_by_id('n34')
+  expect(pn.type).toBe('SEQ')
+  expect(yd.get_node_by_id('n33').value).toBe('violette')
+  expect(pn.get(1)).toBe('violette')
+  expect(yd.remove_node_by_id('n33')).toBeTruthy()
+  expect(yd.get_node_by_id('n33')).toBeFalsy()
+  expect(pn.get(1)).toBe('brun')
+})
+  
+test('remove yellow node (PAIR of MAP)', () => {
+  expect( yd.get_node_by_id('n3').key.value ).toBe('yellow')
+  expect( yd.remove_node_by_id('n3') ).toBeTruthy()
+  expect( yd.get_node_by_id('n3')).toBeFalsy()
+  expect( yd.get_node_by_id('n30')).toBeFalsy() // 'bleu' node a child of deleted 'yellow' Pair
+})
+
+test('create node', () =>{
+  nod = yd.create_node( { new: 'before b' })
+  expect(nod).toBeTruthy()
+  expect(nod.id).toBeTruthy()
+  expect(nod.type).toBe('MAP')
+})
+
+test('insert node before azul/b', () => {
+  let nod = yd.create_node( { new: 'before b' })
+  let at = yd.get_node_by_id('n9')
+  expect( at.type ).toBe('SEQ')
+  expect( at.get(1) ).toBe('b')
+  expect( yd.insert_at_id('n11',nod,true)).toBeTruthy()
+  expect( yd.get_parent_by_id('n6').getIn(['azul',1]).get('new') ).toBe('before b')
+  expect( at.get(2) ).toBe('b')
+})
+
+test('insert node before azul/a', () => {
+  let nod = yd.create_node( { new: 'before a' })
+  let at = yd.get_node_by_id('n9')
+  expect( at.type ).toBe('SEQ')
+  expect( at.get(0) ).toBe('a')
+  expect( yd.insert_at_id('n10',nod,true)).toBeTruthy()
+  expect( yd.get_parent_by_id('n6').getIn(['azul',0]).get('new') ).toBe('before a')
+  expect( at.get(1) ).toBe('a')
+  expect
+})
+
+test('insert node after azul/a', () => {
+  let nod = yd.create_node( { new: 'after a' })
+  let at = yd.get_node_by_id('n9')
+  expect( at.type ).toBe('SEQ')
+  expect( at.get(1) ).toBe('a')
+  expect( yd.insert_at_id('n10',nod)).toBeTruthy()
+  expect( yd.get_parent_by_id('n6').getIn(['azul',2]).get('new') ).toBe('after a')
+  expect( at.get(1) ).toBe('a')
+})
+
+test('insert pair node after amarillo', () => {
+  let nod = yd.create_pair_node( 'new', { boog: 'after amarillo' })
+  let at = yd.get_node_by_id('n7')
+  let pn = yd.get_parent_by_id('n7')
+  expect( at.type ).toBe('PAIR')
+  expect( pn.type ).toBe('MAP')
+  expect( at.key.value ).toBe('amarillo')
+  expect( pn.items[2].key.value ).toBe('amarillo')
+  expect( yd.insert_at_id('n7', nod) ).toBeTruthy()
+  expect( pn.items[2].key.value ).toBe('amarillo')
+  expect( pn.items[3].key.value ).toBe('new')
+  expect( pn.items[3].value.type ).toBe('MAP')
+})
+
+test('insert pair node before amarillo', () => {
+  let nod = yd.create_pair_node( 'new_before', { boog: 'after amarillo' })
+  let at = yd.get_node_by_id('n7')
+  let pn = yd.get_parent_by_id('n7')
+  expect( at.type ).toBe('PAIR')
+  expect( pn.type ).toBe('MAP')
+  expect( at.key.value ).toBe('amarillo')
+  expect( pn.items[2].key.value ).toBe('amarillo')
+  expect( yd.insert_at_id('n7', nod, true) ).toBeTruthy()
+  expect( pn.items[2].key.value ).toBe('new_before')
+  console.log( pn.items[2] )
+  expect( pn.items[2].value.type).toBe('MAP')
+  expect( pn.items[3].key.value ).toBe('amarillo')
+})
+
+test('append pair to blue (PAIR to MAP)', () => {
+  let ato = yd.get_node_by_id('n2')
+  let nod = yd.create_pair_node("new_append", [1,2,3])
+  expect(ato.key.value).toBe('blue')
+  expect(nod.value.type).toBe('SEQ')
+  expect(ato.value.type).toBe('MAP')
+  console.log(nod)
+  expect( yd.append_to_id(ato.value.id, nod) ).toBeTruthy()
+  expect( nod.parent_id).toBe(ato.value.id)
+  expect( yd.get_parent_by_id(nod.id) ).toEqual( ato.value )
+  expect( ato.value.items[ato.value.items.length-1] ).toEqual( nod )
+})
+
+test('prepend scalar to gelb (PLAIN to SEQ)', () => {
+  let ato = yd.get_node_by_id('n17')
+  let nod = yd.create_node("new_prepend")
+  console.log(nod)
+  expect(ato.key.value).toBe('gelb')
+  expect(nod.type).toBe('PLAIN')
+  expect(ato.value.type).toBe('SEQ')
+  expect( yd.append_to_id(ato.value.id, nod, true) ).toBeTruthy()
+  expect( nod.parent_id).toBe(ato.value.id)
+  expect( yd.get_parent_by_id(nod.id) ).toEqual( ato.value )
+  expect( ato.value.items[0] ).toEqual( nod )
+})
